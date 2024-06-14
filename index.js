e8df2cdc3b0a7d45d46ceda19a6c616a6252f430
Oleksii_Andriushchenko <oa@readable.games>
2024-06-14 11:43:32 +0300

    added checkbox buttons

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const content = document.getElementById("content");
 const generate = document.getElementById("generate");
 const clear = document.getElementById("clear");
 
+const uppercase = document.getElementById("uppercase");
+const lowercase = document.getElementById("lowercase");
+const symbols = document.getElementById("symbols");
+const numbers = document.getElementById("numbers");
+
 const passworLength = 8;
-const isUpperCase = true;
-const isLowerCase = true;
-const isSymbol = true;
-const isNumber = true;
 
 generate.onclick = function() {
 
@@ -19,21 +20,32 @@ generate.onclick = function() {
     const symbolChars = "!@#$%^&*()_+-";
     const numberChars = "1234567890";
 
-    allCharsInPassword += isUpperCase ? upperCaseChars : "";
-    allCharsInPassword += isLowerCase ? lowerCaseChars : "";
-    allCharsInPassword += isSymbol ? symbolChars : "";
-    allCharsInPassword += isNumber ? numberChars : "";
+    allCharsInPassword += uppercase.checked ? upperCaseChars : "";
+    allCharsInPassword += lowercase.checked ? lowerCaseChars : "";
+    allCharsInPassword += symbols.checked ? symbolChars : "";
+    allCharsInPassword += numbers.checked ? numberChars : "";
 
-for(let i = 0; i < passworLength; i++) {
-    let randomIndex = Math.floor(Math.random() * allCharsInPassword.length)
-    password += allCharsInPassword[randomIndex]
-}
+    if(!uppercase.checked && !lowercase.checked && !symbols.checked && !numbers.checked){
+        content.textContent = "You should choose at least one options"
+    } else {
+
+        for(let i = 0; i < passworLength; i++) {
+            let randomIndex = Math.floor(Math.random() * allCharsInPassword.length)
+            password += allCharsInPassword[randomIndex]
+        }
+            
+        
+        content.textContent = password;
+    }
     
 
-content.textContent = password;
 
 }
 
-clear.onclick = function () {
+clear.addEventListener("click", function () {
     content.textContent = "Press Generate to create uniq random password"
-}
+    uppercase.checked = false;
+    lowercase.checked = false;
+    symbols.checked = false;
+    numbers.checked = false;
+})
